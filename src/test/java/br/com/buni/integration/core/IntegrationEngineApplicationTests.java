f718acbd8785e8b2c23d653768fc2bb6e978a2af
alanerochaa <alanerochaa@gmail.com>
2026-07-19 23:15:47 -0300

    refactor: reorganiza perfis de ambiente e configuração de testes

--- src/test/java/br/com/buni/integration/core/IntegrationEngineApplicationTests.java
+++ src/test/java/br/com/buni/integration/core/IntegrationEngineApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.buni.integration.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class IntegrationEngineApplicationTests {
 
 	@Test
